cfc1b3916f898be3922bf41dc82ac605fb27ae92
learnuidev <learnuidev@gmail.com>
2025-10-20 02:52:31 -0400

    wip

--- src/utils/add-event-function.ts
+++ src/utils/add-event-function.ts
@@ -1,8 +1,10 @@
 import type { IAddEventInput, IEvent } from "@/types/event.types";
 
 export const addEventFunction = (event: IAddEventInput): IEvent => {
-	return {
-		...event,
-		createdAt: Date.now(),
-	};
+	// return {
+	// 	...event,
+	// 	createdAt: Date.now(),
+	// };
+
+	return 1 + 1
 };
