c8bae07ecfc3f6acc03f94849b996e81abaa3918
learnuidev <learnuidev@gmail.com>
2025-10-22 12:40:47 -0400

    make sure collection is array

--- src/utils/map.ts
+++ src/utils/map.ts
@@ -1,1 +1,6 @@
-export const map = <T, U>(coll: T[], fn: (item: T) => U) => coll.map(fn);
+export const map = <T, U>(coll: T[], fn: (item: T) => U) => {
+	if (!Array.isArray(coll)) {
+		throw new Error("Expected an array as the first parameter");
+	}
+	return coll.map(fn);
+};
